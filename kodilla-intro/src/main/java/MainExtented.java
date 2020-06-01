67991aa67b0a3fe27e0bddb896e9f24bb75944e8
Agnieszka Miller <agnieszka.a.miller@wp.pl>
2020-06-01 11:03:20 +0200

    Initial version

---
+++ kodilla-intro/src/main/java/MainExtented.java
@@ -0,0 +1,16 @@
+public class MainExtented {
+    public static void main(String[] args) {
+        String myDog = "Azor";
+        String breedOfDog = "labrador";
+        int dogsAge = 12;
+        double weight = 10.50;
+        char dogsFirstNameLetter = 'A';
+        boolean isGoodDog = true;
+
+        System.out.println(myDog);
+        System.out.println(breedOfDog);
+        System.out.println(dogsAge);
+        System.out.println(weight);
+        System.out.println(dogsFirstNameLetter);
+    }
+}
