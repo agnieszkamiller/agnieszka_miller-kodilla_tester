35ad168888dbf04f54d5ae5fe4ef437eebdc1763
Agnieszka Miller <agnieszka.a.miller@wp.pl>
2020-07-10 11:16:32 +0200

    Initial version

--- kodilla-stream/src/main/java/com/kodilla/stream/homework/ForumStats.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/homework/ForumStats.java
@@ -7,19 +7,14 @@ import java.util.stream.Collectors;
 
 public class ForumStats {
     public static void main(String[] args) {
-        //uruchom Stream na kolekcji użytkowników
-        // zwracanej przez UsersRepository
         double result = UserRepository.getUsersList()
-
-        //Stream obliczający średnią liczbę postów dla użytkowników,
-        // których wiek jest >= 40.
 
                 .stream()
                 .map(u ->u.getNumberOfPosts())
-                .mapToInt(n -> n)//Integer --> int
-                .average()// kolektor average()
-                .getAsDouble();//OptionalDouble --> double
-        System.out.println("Srednia postów wszystkich: " + result);
+                .mapToInt(n -> n)
+                .average()//
+                .getAsDouble();
+        System.out.println("Srednia postów wszystkich osób: " + result);
 
         double resultForYounger = UserRepository.getUsersList()
                 .stream()
@@ -36,20 +31,6 @@ public class ForumStats {
                 .average()
                 .getAsDouble();
         System.out.println("Srednia postów dla osób po 40.: " + resultForAdults);
-
-        double avar = UserRepository.getUsersList()
-                .stream()
-                .filter(value -> value.getAge() < 40)
-                .collect(Collectors.averagingDouble(User::getNumberOfPosts));
-
-        System.out.println(avar);
-
-
-        double avar1 = UserRepository.getUsersList()
-                .stream()
-                .filter(value -> value.getAge() < 40)
-                .collect(Collectors.summarizingInt(User::getNumberOfPosts)).getAverage();
-        System.out.println(avar1);
 
     }
 }
