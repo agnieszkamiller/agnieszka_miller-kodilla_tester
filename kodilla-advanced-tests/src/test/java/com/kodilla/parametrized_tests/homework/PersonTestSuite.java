fdd3c8b2e8169bc5366943ee2864c32aca170a0d
Agnieszka Miller <agnieszka.a.miller@wp.pl>
2020-07-31 22:50:12 +0200

    Initial version

--- kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/PersonTestSuite.java
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/PersonTestSuite.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class SubscriberTestSuite {
+class PersonTestSuite {
 
     @ParameterizedTest
     @MethodSource(value = "com.kodilla.parametrized_tests.homework.DataSourceBMI#provideDataToBMICalculator")
