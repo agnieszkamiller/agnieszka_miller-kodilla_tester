8d8b28f510c286a2a111514fc35cbdf1bfb0e92c
Agnieszka Miller <agnieszka.a.miller@wp.pl>
2020-08-19 13:16:06 +0200

    9.5

--- kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_scopes/homework/Clock.java
+++ kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_scopes/homework/Clock.java
@@ -2,14 +2,12 @@ package com.kodilla.spring.basic.spring_scopes.homework;
 
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
-
-
 import java.time.LocalTime;
+
 @Component
 @Scope("prototype")
 public class Clock {
     private LocalTime localTime;
-
 
     public LocalTime getLocalTime() {
         return localTime;
