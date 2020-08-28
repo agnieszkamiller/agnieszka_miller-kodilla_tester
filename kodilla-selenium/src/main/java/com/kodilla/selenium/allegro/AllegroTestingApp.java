8294e5e1b40098da0b645a06635069b49e9c2956
Agnieszka Miller <agnieszka.a.miller@wp.pl>
2020-08-28 12:08:06 +0200

    13.4.

--- kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestingApp.java
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestingApp.java
@@ -5,9 +5,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import org.openqa.selenium.support.ui.Select;
-
 public class AllegroTestingApp {
+
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver", "c:\\selenium-drivers\\chrome\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
@@ -24,20 +23,6 @@ public class AllegroTestingApp {
 
         WebElement szukajButton = driver.findElement(By.xpath("//*[@type=\"submit\"]"));
         szukajButton.click();
-
-
-//        WebElement webElement = driver.findElement(By.xpath("//button[@data-role='accept-consent']"));
-//        webElement.click();
-//
-//        WebElement eleCombo = driver.findElement(By.xpath("//select[@data-role='filters-dropdown-toggle']"));
-//        Select eleSelect = new Select(eleCombo);
-//        eleSelect.selectByIndex(3);
-//
-//        WebElement input = driver.findElement(By.xpath("//input[@role='combobox']"));
-//        input.sendKeys("Mavic mini");
-//
-//        WebElement szukajButton = driver.findElement(By.xpath("//button[@data-role='search-button']"));
-//        szukajButton.click();
 
     }
 }
