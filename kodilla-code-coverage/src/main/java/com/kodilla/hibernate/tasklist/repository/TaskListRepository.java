a4006f4381b6b8b2b601f4b9b6167a40e2dc1285
Agnieszka Miller <agnieszka.a.miller@wp.pl>
2020-09-17 15:17:26 +0200

    15.4

--- kodilla-code-coverage/src/main/java/com/kodilla/hibernate/tasklist/repository/TaskListRepository.java
+++ kodilla-code-coverage/src/main/java/com/kodilla/hibernate/tasklist/repository/TaskListRepository.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
 import java.util.List;
+
 @Repository
 @Transactional
 public interface TaskListRepository extends CrudRepository<TaskList, Integer> {
--- kodilla-code-coverage/src/test/java/com/kodilla/hibernate/tasklist/repository/TaskListRepositoryTestSuite.java
+++ kodilla-code-coverage/src/test/java/com/kodilla/hibernate/tasklist/repository/TaskListRepositoryTestSuite.java
@@ -1,11 +1,38 @@
 package com.kodilla.hibernate.tasklist.repository;
 
+import com.kodilla.hibernate.tasklist.TaskList;
+import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class TaskListRepositoryTestSuite {
+    @Autowired
+    private TaskListRepository taskListRepository;
+    private String listName = "First List";
+    private String description = "Here find full description";
 
     @Test
-    public void testFindByListName(){
-//czy metoda findByListName(String listName) działa poprawnie.
+    public void testFindByListName() {
+        //Given
+        TaskList taskList = new TaskList(listName, description);
+        taskListRepository.save(taskList);
+        listName = taskList.getListName();
+
+        //When
+        List<TaskList> readTasksList = taskListRepository.findByListName(listName);
+
+        //Then
+        Assert.assertEquals(1, readTasksList.size());
+
+        //CleanUp
+        int id = readTasksList.get(0).getId();
+        taskListRepository.deleteById(id);
     }
 }
